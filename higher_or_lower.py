e22bff8a869a76312157ebae2d7f6fc6973d1e18
bagayokom <bagayokom@hartwick.edu>
2019-10-30 17:03:43 -0400

    first version

--- higher_or_lower.py
+++ higher_or_lower.py
@@ -4,12 +4,12 @@ import random
 # Higher or Lower Game
 # November 5 2019
 
-
+print("naruto gang")
 def introduction():
     print("Hello user. My name is the dealer. Please enter your username:")
     username = input("> ")
     print(f"""Ladies and gentlemen it's showtime!
-Welcome to Higher or Lower {username}!""")
+Welcome to Higher or Lower, {username}!""")
 
 def rules():
     print("""Do you want me to explain the rules?
@@ -17,7 +17,7 @@ def rules():
     rule = int(input("> "))
     if rule == 1:
         print("""----------------------------------------------------------
-You will start off with 30 point total. I will  draw a number between 10-21. 
+You will start off with 30 point total. I will draw a number between 10-21. 
 I will then reveal the number to you. Then this is where it gets interesting! 
 I will draw a second number between 10-21. However, I will not reveal the number immediately. 
 This is where you will step in. You will then have to guess if the second number is higher or lower than the first number. 
